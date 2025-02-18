dd5e2b9632cd522c395bd418e1810ab20b6b859a
xeeble <xeeble@hotmail.co.uk>
2025-02-18 11:06:44 -0500

    dealer details and reviews using sentiment analyzer implemented

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 
+
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
@@ -15,9 +16,19 @@ urlpatterns = [
     path(route='logout', view=views.logout_request, name='logout'),
 
     # path for cars
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    path(route='get_cars', view=views.get_cars, name ='get_cars'),
+    
+    # path for getting dealerships
+    path(route='get_dealers', view=views.get_dealerships, name ='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+
+    # path for getting dealerships by ID
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    
     # path for dealer reviews view
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
 
     # path for add a review view
+    path(route='add_review', view=views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
