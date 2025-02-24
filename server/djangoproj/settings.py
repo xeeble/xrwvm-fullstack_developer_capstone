d8893cf99b26e83aba4b2706b020696b8cca7f41
GXSOne <omgdzyt@gmail.com>
2025-02-24 15:09:56 +0000

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -20,9 +20,9 @@ SECRET_KEY =\
     'django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['localhost', 'https://omgdzyt-8000.theianext-1-labs-' + \
+ALLOWED_HOSTS = ['localhost', 'https://omgdzyt-8000.theianext-1-labs-' +
                  'prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://omgdzyt-8000.theiadockernext-0-labs-' + \
+CSRF_TRUSTED_ORIGINS = ['https://omgdzyt-8000.theiadockernext-0-labs-' +
                         'prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -77,7 +77,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.'+
+        'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
